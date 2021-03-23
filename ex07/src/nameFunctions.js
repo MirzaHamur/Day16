14796cf4e1981763948806105d9de8f9aa7510b3
Mirza <mirza.hamurko@gmail.com>
2021-03-24 00:46:31 +0100

    ex07

---
+++ ex07/src/nameFunctions.js
@@ -0,0 +1,11 @@
+function firstName(firstName) {
+    return firstName.toUpperCase();
+  }
+   //Functions
+  function lastName(lastName) {
+    return lastName.toLowerCase();
+  }
+  module.exports = {
+    firstName,
+    lastName,
+  };
